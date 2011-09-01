9b50076621073ea8708adc2011f9e4f2ccf043bc
Ondra Gersl <ondra.gersl@gmail.com>
2011-09-01 12:36:03 +0200

    Kod uvolnen pod GPLv3, presunuto TODO.

--- test.c
+++ test.c
@@ -9,6 +9,22 @@
  *            Napr.: [1012222121310101]4=32 (prevod ze 4-kove do 32 soustavy)
  */
 
+/*
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
